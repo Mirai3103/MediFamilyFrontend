d81202d9250904cc403854d7d501e32b8aabfa2e
Huu Hoang <huuhoag1412@gmail.com>
2025-05-15 18:41:55 +0700

    feat: add Google login functionality and enhance callback handling

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,28 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		TanStackRouterVite({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		react(),
-		tsconfigPaths(),
-	],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:8080",
-				changeOrigin: true,
-			},
-		},
-		host: "0.0.0.0",
-	},
+  plugins: [
+    tailwindcss(),
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
+  server: {
+	port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/oauth2": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+
+    host: "0.0.0.0",
+  },
 });
